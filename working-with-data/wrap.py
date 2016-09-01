9cf786cef734ec651a7d0ad700a171c0e1d7e302
mekicha <emeka.icha@gmail.com>
2016-09-01 20:38:58 +0300

    Wrap lines of a file longer than width

---
+++ working-with-data/wrap.py
@@ -0,0 +1,17 @@
+"""Write a program wrap.py that takes filename and width as
+aruguments and wraps the lines longer than width. """
+
+
+
+def wrap(filename, delim):
+ 	string = ''
+ 	with open(filename) as file:
+ 		    string += file.read().strip()
+ 	while string:
+ 		s = string[:delim]
+ 		string = string[delim:]
+ 		print s
+
+
+
+print wrap('she.txt', 20)
